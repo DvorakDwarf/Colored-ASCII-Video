df2555c8229fedd6948e9c617fd9c6f7760c7f24
Roman <spiderr.roma@gmail.com>
2023-02-11 21:31:41 +0200

    its my first time doing python fixed the slowness

--- downloadVid.py
+++ downloadVid.py
@@ -6,8 +6,7 @@ import math
 
 def download_video(video_url):
     yt = YouTube(video_url)
-    stream = yt.streams.filter(file_extension='mp4').first()
-    response = requests.get(stream.url, stream=True)
+    
     filename = yt.title + ".mp4"
     file_path = os.path.join("Video/", filename)
 
@@ -20,6 +19,9 @@ def download_video(video_url):
     total_size = int(response.headers.get("Content-Length", 0))
     block_size = 1024
     wrote = 0 
+    
+    stream = yt.streams.filter(file_extension='mp4').first()
+    response = requests.get(stream.url, stream=True)
 
     with open(file_path, "wb") as f:
         for data in tqdm(response.iter_content(block_size), total=math.ceil(total_size//block_size), unit="KB", unit_scale=True):
