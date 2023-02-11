6e3aabf66ffbaa59b2a1e4286789c38464c1912a
Roman <spiderr.roma@gmail.com>
2023-02-11 21:38:13 +0200

    sorry again

--- downloadVid.py
+++ downloadVid.py
@@ -16,13 +16,13 @@ def download_video(video_url):
         video_url = file_path
         return video_url
 
-    total_size = int(response.headers.get("Content-Length", 0))
-    block_size = 1024
-    wrote = 0 
-    
     stream = yt.streams.filter(file_extension='mp4').first()
     response = requests.get(stream.url, stream=True)
 
+    total_size = int(response.headers.get("Content-Length", 0))
+    block_size = 1024
+    wrote = 0 
+     
     with open(file_path, "wb") as f:
         for data in tqdm(response.iter_content(block_size), total=math.ceil(total_size//block_size), unit="KB", unit_scale=True):
             wrote = wrote  + len(data)
